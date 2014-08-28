315cdacd0b28d9aeb2a4458a88a0d2d28f3e5588
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-28 12:37:06 -0700

    Add Post class for holding all information/variables related to a Post

--- src/ssk/project/reddit_reader/Post.java
+++ src/ssk/project/reddit_reader/Post.java
@@ -2,6 +2,19 @@ package ssk.project.reddit_reader;
 
 public class Post {
 
-	String title, author, comment;
-	int points, numOfComments;
+	String subreddit, title, author, permalink, url, domain, id;
+	int points, numComments;
+	
+	String getTitle() {
+		return title;
+	}
+	
+	String getScore() {
+		return "" + points;
+	}
+	
+	String getDetails() {
+		String details = author + " posted this and got " + numComments + " replies";
+		return details;
+	}
 }
