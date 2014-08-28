a9333a84bae32e3c2049d9844c1160d91fb50fb4
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-28 12:33:32 -0700

    Added xml layouts

--- src/ssk/project/reddit_reader/MainActivity.java
+++ src/ssk/project/reddit_reader/MainActivity.java
@@ -1,22 +1,22 @@
 package ssk.project.reddit_reader;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addFragment();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void addFragment() {
+		getSupportFragmentManager()
+			.beginTransaction()
+			.add(R.id.fragments_holder, PostsFragment.newInstance("keto"))
+			.commit();
 	}
 
 }
